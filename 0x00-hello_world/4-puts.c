ec6a4875de2c4de4bfb6083f06dd95e8c892bb5b
yarmakx <yarmakx@gmail.com>
2020-02-05 20:59:03 +0000

    modify script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-	char msg[100]="\"Programming is like building a multilingual puzzle";
-	puts (msg);
+	char msg[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(msg);
 	return (0);
 }
