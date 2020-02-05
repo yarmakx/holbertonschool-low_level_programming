7365a66afeefc76fef7f1ef9da9ef4a1b6d61d89
yarmakx <yarmakx@gmail.com>
2020-02-05 21:06:27 +0000

    modify script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
-* main-prints "Programming is like building a multilingual puzzle
-*
-* Return: 0 
-*/
-
+ * main-prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char msg[100] = "\"Programming is like building a multilingual puzzle";
