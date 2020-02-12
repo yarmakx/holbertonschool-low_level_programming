e088cb4a5c6f0c366606829c73a0357475e34c40
yarmakx <yarmakx@gmail.com>
2020-02-12 00:17:59 +0000

    modify natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,6 +4,7 @@
  * main - list all the natural numbers below 1024 that are multiples of 3 or 5
  * @n: natural numer
  * @sum: the sum
+ * Return: 0
  */
 int main(void)
 {
@@ -14,5 +15,6 @@ int main(void)
 	if ((n % 3 == 0) || (n % 5 == 0))
 	sum = sum + n;
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
+	return (0);
 }
