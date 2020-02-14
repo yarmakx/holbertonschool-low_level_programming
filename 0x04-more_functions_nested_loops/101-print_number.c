456ccf95e49126fe60ce9af08eceb9ab1866d066
yarmakx <yarmakx@gmail.com>
2020-02-14 00:06:13 +0000

    add print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  * print_number - a function that prints an integer
+  * @n: integer
+  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n/10)
+		print_number(n/10);
+	_putchar(n % 10 + '0');
+}
