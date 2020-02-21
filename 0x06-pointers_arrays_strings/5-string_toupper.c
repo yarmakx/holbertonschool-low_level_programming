72123b5a13015c036f432c163a3291fb7614845c
yarmakx <yarmakx@gmail.com>
2020-02-21 04:08:34 +0000

    add 5 function

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 
 /**
-  * string_toupper - a function that changes 
+  * string_toupper - a function that changes
+  * @a: an array
   * all lowercase letters of a string to uppercase
+  * Return: a
   */
 char *string_toupper(char *a)
 {
 	int i;
-	
+
 	i = 0;
 	while (a[i] != '\0')
 	{
