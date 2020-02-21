72993cb72ea5ba96a7454546f75fb7c3d54159e1
yarmakx <yarmakx@gmail.com>
2020-02-21 00:59:44 +0000

    modify strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,27 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, n;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		if (s1[i] == s2[i])
-			return (0);
+		{
+			n = s1[i] - s2[i];
+		break;
+		}
+		else if (s1[i] > s2[i])
+		{
+			n = s1[i] - s2[i];
+		break;
+		}
+		else if (s1[i] == s2[i])
+		{
+			n = 0;
+		break;
+		}
 		i++;
 	}
-	return (0);
+	return (n);
 }
